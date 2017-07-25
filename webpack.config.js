7c524707b88cd8928c3cf3788ad6e0ba0bd78d96
Sandoche Adittane <sandoche@gmail.com>
2017-07-25 17:25:12 +0200

    browser sync watching files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -13,7 +14,26 @@ module.exports = {
       template: './src/template/default.html',
       filename: '../_layouts/default.html',
       title: 'Dev mode'
-    })
+    }),
+    new BrowserSyncPlugin(
+      // BrowserSync options
+      {
+        // browse to http://localhost:3000/ during development
+        host: 'localhost',
+        port: 3000,
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:3100/)
+        // through BrowserSync
+        proxy: 'http://localhost:8080',
+        files: ["_site/*.html"]
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: true
+      }
+    )
   ],
   devServer: {
     contentBase: [

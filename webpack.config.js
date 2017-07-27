d1521f785b34c296ea496eabb49c5ec5b68151ca
Sandoche Adittane <sandoche@gmail.com>
2017-07-27 21:33:29 +0200

    reload changed to false

--- webpack.config.js
+++ webpack.config.js
@@ -16,22 +16,14 @@ module.exports = {
       title: 'Dev mode'
     }),
     new BrowserSyncPlugin(
-      // BrowserSync options
       {
-        // browse to http://localhost:3000/ during development
         host: 'localhost',
         port: 3000,
-        // proxy the Webpack Dev Server endpoint
-        // (which should be serving on http://localhost:3100/)
-        // through BrowserSync
         proxy: 'http://localhost:8080',
         files: ["_site/*.html"]
       },
-      // plugin options
       {
-        // prevent BrowserSync from reloading the page
-        // and let Webpack Dev Server take care of this
-        reload: true
+        reload: false
       }
     )
   ],

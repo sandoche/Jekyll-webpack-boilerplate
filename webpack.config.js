fbfffd96993d4697b33042583401dcc653a92656
Sandoche Adittane <sandoche@gmail.com>
2017-07-28 12:09:51 +0200

    hmr added

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -25,12 +26,14 @@ module.exports = {
       {
         reload: false
       }
-    )
+    ),
+    new webpack.HotModuleReplacementPlugin()
   ],
   devServer: {
     contentBase: [
       './_site'
-    ]
+    ],
+    hot: true
   },
   output: {
     filename: '[name].bundle.js',

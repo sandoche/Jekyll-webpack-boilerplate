454b97b6045e46b0e6a5efe4b97543aec1c466fa
Sandoche Adittane <sandoche@gmail.com>
2017-08-05 21:06:50 +0200

    webpack critical css path added

---
+++ config/webpack.optim.js
@@ -0,0 +1,18 @@
+const CriticalPlugin = require('webpack-plugin-critical').CriticalPlugin;
+
+module.exports = {
+  entry: './config/optimization-fix/do-not-delete.js',
+  output: {
+    filename: 'optimization-fix-this-file-is-ignored.js'
+  },
+  plugins: [
+    new CriticalPlugin({
+      base: '../_layouts/',
+      src: 'default.html',
+      inline: true,
+      minify: true,
+      dest: '_layouts/default.html',
+      css: ['_site/assets/app.css']
+    })
+  ]
+}

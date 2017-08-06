7fa2332b1c826a47877bb78d8e789fa6322bf264
Sandoche Adittane <sandoche@gmail.com>
2017-08-06 21:35:06 +0200

    pwa script improved

---
+++ config/sw.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "staticFileGlobs": [
+    "_site/assets/**.css",
+    "_site/**.html",
+    "_site/assets/images/**.*",
+    "_site/assets/**.js"
+  ],
+  "stripPrefix": "_site/",
+  "runtimeCaching": [{
+    "urlPattern": "/",
+    "handler": "networkFirst"
+  }],
+  "root": "_site"
+}
